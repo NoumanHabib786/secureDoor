3fc65a62798661fc635b397bd11d8f4d482fb617
nouman <noumanhabibi0348@gmail.com>
2024-02-25 07:00:20 +0500

    Nouman

--- lib/resources/imagesFolder.dart
+++ lib/resources/imagesFolder.dart
@@ -2,6 +2,8 @@ import 'package:secure_fitness_comp/resources/appColors.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:secure_fitness_comp/resources/imagesPaths.dart';
+import 'package:sizer/sizer.dart';
 
 import 'components/Buttons.dart';
 
@@ -21,13 +23,15 @@ class ImagesFolder {
   static String netImage =
       "https://img.freepik.com/free-vector/front-car-concept-illustration_114360-7978.jpg?w=900&t=st=1696244645~exp=1696245245~hmac=742ad4c5811a04e1f1190e82b7784ee967ad3958392a58419a0950900125f737";
 
-  static CachedNetworkImage getImage({required String url , }) {
+  static CachedNetworkImage getImage({
+    required String url,
+  }) {
     return CachedNetworkImage(
       imageUrl: url,
       placeholder: (context, url) => loading(),
-      errorWidget: (context, url, error) => const Icon(
-        Icons.info,
-        color: AppColors.mainColor,
+      errorWidget: (context, url, error) => Image.asset(
+        ImagesPaths.demo,
+        height: 4.h,
       ),
       fit: BoxFit.cover,
     );
